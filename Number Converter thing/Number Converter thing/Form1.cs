e701c44a51eb114ec81b6b788ef8298a6d669a9f
Kai <kedup432@learner.dvusd.org>
2025-01-14 09:30:13 -0700

    Added more bits

--- Number Converter thing/Number Converter thing/Form1.cs
+++ Number Converter thing/Number Converter thing/Form1.cs
@@ -14,6 +14,7 @@ namespace Number_Converter_thing
     {
         int total = 0;
         int[] bits = new int[8];
+        int[] bits2 = new int[8];
         public Form1()
         {
             InitializeComponent();
@@ -47,6 +48,34 @@ namespace Number_Converter_thing
                 total += 128;
             TotalLabel.Text = total.ToString();
         }
+        private void Calculate()
+        {
+            total = 0;
+            if (bits2[0] != 0)
+                total += 1;
+            //
+            if (bits2[1] != 0)
+                total += 2;
+            //
+            if (bits2[2] != 0)
+                total += 4;
+            //
+            if (bits2[3] != 0)
+                total += 8;
+            //
+            if (bits2[4] != 0)
+                total += 16;
+            //
+            if (bits2[5] != 0)
+                total += 32;
+            //
+            if (bits2[6] != 0)
+                total += 64;
+            //
+            if (bits2[7] != 0)
+                total += 128;
+            TotalLabel2.Text = total.ToString();
+        }
         private void Bit8_TextChanged(object sender, EventArgs e)
         {
             if (Bit1.Text == "1")
@@ -55,40 +84,86 @@ namespace Number_Converter_thing
                 bits[0] = 0;
             //
             if (Bit2.Text == "1")
-                bits[0] = 1;
+                bits[1] = 1;
             else
-                bits[0] = 0;
+                bits[1] = 0;
             //
             if (Bit3.Text == "1")
-                bits[0] = 1;
+                bits[2] = 1;
             else
-                bits[0] = 0;
+                bits[2] = 0;
             //
             if (Bit4.Text == "1")
-                bits[0] = 1;
+                bits[3] = 1;
             else
-                bits[0] = 0;
+                bits[3] = 0;
             //
             if (Bit5.Text == "1")
-                bits[0] = 1;
+                bits[4] = 1;
             else
-                bits[0] = 0;
+                bits[4] = 0;
             //
             if (Bit6.Text == "1")
-                bits[0] = 1;
+                bits[5] = 1;
             else
-                bits[0] = 0;
+                bits[5] = 0;
             //
             if (Bit7.Text == "1")
-                bits[0] = 1;
+                bits[6] = 1;
             else
-                bits[0] = 0;
+                bits[6] = 0;
             //
             if (Bit8.Text == "1")
-                bits[0] = 1;
+                bits[7] = 1;
             else
-                bits[0] = 0;
+                bits[7] = 0;
             Calc();
+        }
+
+        private void BitB8_TextChanged(object sender, EventArgs e)
+        {
+            {
+                if (BitB1.Text == "1")
+                    bits2[0] = 1;
+                else
+                    bits2[0] = 0;
+                //
+                if (BitB2.Text == "1")
+                    bits2[1] = 1;
+                else
+                    bits2[1] = 0;
+                //
+                if (BitB3.Text == "1")
+                    bits2[2] = 1;
+                else
+                    bits2[2] = 0;
+                //
+                if (BitB4.Text == "1")
+                    bits2[3] = 1;
+                else
+                    bits2[3] = 0;
+                //
+                if (BitB5.Text == "1")
+                    bits2[4] = 1;
+                else
+                    bits2[4] = 0;
+                //
+                if (BitB6.Text == "1")
+                    bits2[5] = 1;
+                else
+                    bits2[5] = 0;
+                //
+                if (BitB7.Text == "1")
+                    bits2[6] = 1;
+                else
+                    bits2[6] = 0;
+                //
+                if (BitB8.Text == "1")
+                    bits2[7] = 1;
+                else
+                    bits2[7] = 0;
+                Calculate();
+            }
         }
     }
 }
